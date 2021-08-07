ab1fedf0640ab58aa62edcfe25224dfcb7abd4a1
Surya Rajendhran <cielograndesurya@gmail.com>
2021-08-07 20:53:21 +0530

    Update metadata

--- gatsby-config.js
+++ gatsby-config.js
@@ -65,9 +65,9 @@ module.exports = {
     {
       resolve: `gatsby-plugin-manifest`,
       options: {
-        name: `minimal-blog - @lekoarts/gatsby-theme-minimal-blog`,
-        short_name: `minimal-blog`,
-        description: `Typography driven, feature-rich blogging theme with minimal aesthetics. Includes tags/categories support and extensive features for code blocks such as live preview, line numbers, and code highlighting.`,
+        name: `Surya Rajendhran`,
+        short_name: `Surya Rajendhran`,
+        description: `A place internet strangers can get to know me.`,
         start_url: `/`,
         background_color: `#fff`,
         theme_color: `#6B46C1`,
